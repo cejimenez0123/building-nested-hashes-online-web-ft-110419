d2f1b18a576ebdca19310c2e3376d9594e3416d7
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-13 20:56:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {patriarch:{},
       matriarch:{},
       hero:{},
-      hero_friends:{}
+      hero_friends:[]
       #your key/value pairs here!
       },
     :capulet => {patriarch:{},
