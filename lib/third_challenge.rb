e72577b2892798d31e1fc81a67ae9c06427934c0
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-13 21:00:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,7 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name:"",age:""
+      :patriarch => {name:"Lord Montague",age:"53"
         #your key/value pairs here
         },
       :matriarch => {name:"",age:""
