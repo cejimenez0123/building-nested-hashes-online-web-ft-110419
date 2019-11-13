173b18db655f03811911f00ea178d1f6ffe40477
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-13 20:57:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
     :capulet => {patriarch:{},
                 matriarch:{},
                 heroine:{},
-                heroine_friends:{}
+                heroine_friends:[]
       #your key/value pairs here!
       }}
 
